5a132aa7577e28799badf0fac7ca85d42680b4e7
vishwateja-code <vishwatejathallapalli@gmail.com>
2025-06-10 09:19:27 +0530

    Update EmployeeService.js

--- employeemanagement-frontend/src/service/EmployeeService.js
+++ employeemanagement-frontend/src/service/EmployeeService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 //const BASE_URL = process.env.REACT_APP_BACKEND_URL
-const BASE_URL = "http://www.vishwateja.co.in ";
+const BASE_URL = "http://www.vishwateja.co.in";
 // const BASE_URL = process.env.REACT_APP_BASE_URL ; // Fallback URL
 class EmployeeService{
 
